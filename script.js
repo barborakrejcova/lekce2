f8a374706a97d825f167df11995bda223a5e3b81
Barbora Krejčová <krejcova.barbora96@gmail.com>
2024-10-13 16:01:03 +0200

    cviceni 2 hod kostkou

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ prijem = ((studentVstup * studenti) + (vstup * (pocetNavstevniku - studenti))) *
 document.body.innerHTML += "<p>Nový příjem: " + prijem + " Kč</p>";
 
 
+//Cviceni 2 - Hazeni kostkou
+
+/*
+Vymyslete, jak použít funkci Math.random a různé zaokrouhlovací funkce probírané
+v této lekci k simulování hodu klasickou šestistěnnou kostkou.
+S použitím vhodných funkcí sestavte výraz, který vygeneruje náhodné celé číslo mezi 1 a 6.
+
+Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou pravděpodobností.
+Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 0 (včetně) a 1 (vyjma).
+Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. Naopak číslo 1 padnout nemůže.
+*/
+
+const hodKostkou = Math.floor(Math.random() * 6) + 1;
+document.body.innerHTML += "<p>" + hodKostkou + "</p>";
